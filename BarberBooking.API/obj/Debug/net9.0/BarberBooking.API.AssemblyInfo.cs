a446a0bdb39d509bb9d66d6aa8308ba96131b777
artem <artemsv1308@gmail.com>
2026-04-10 12:06:21 +0400

    Add Fix Migration

--- BarberBooking.API/obj/Debug/net9.0/BarberBooking.API.AssemblyInfo.cs
+++ BarberBooking.API/obj/Debug/net9.0/BarberBooking.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberBooking.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff5c1b9a95180c3e9efd196a6aab258c45381e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51090507446a750db5120dbabbe6162d7b80855")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberBooking.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberBooking.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
