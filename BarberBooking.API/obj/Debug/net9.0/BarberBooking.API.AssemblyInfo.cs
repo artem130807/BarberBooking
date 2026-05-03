9252c1e437b7ebf9a5d85b852ae8392e87dfd387
artem1308 <artemsv1308@gmail.com>
2026-05-03 16:52:00 +0400

    Init

--- BarberBooking.API/obj/Debug/net9.0/BarberBooking.API.AssemblyInfo.cs
+++ BarberBooking.API/obj/Debug/net9.0/BarberBooking.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberBooking.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd6e568aaa8c80f79de444d91d3b0bc4618cfad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23fd41f1c1e18045fbc4c796ab2341e7ed95d1cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberBooking.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberBooking.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
