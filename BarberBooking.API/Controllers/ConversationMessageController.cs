c7d0746ea3cfe32c8f702fba4baddfb21926fa25
artem1308 <artemsv1308@gmail.com>
2026-05-03 20:31:43 +0400

    Fix ci/cd yml

--- BarberBooking.API/Controllers/ConversationMessageController.cs
+++ BarberBooking.API/Controllers/ConversationMessageController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BarberBooking.API.CQRS.ConversationMessages.Commands;
+using BarberBooking.API.Dto.DtoConversationMessages;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +17,15 @@ namespace BarberBooking.API.Controllers
         public ConversationMessageController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+        [HttpGet("Create-message")]
+        public async Task<IActionResult> Create(DtoCreateConversationMessage dtoCreateConversationMessage)
+        {
+            var command = new CreateConversationMessageCommand(dtoCreateConversationMessage);
+            var result = await _mediator.Send(command);
+            if(result.IsFailure)
+                return BadRequest(result.Error);
+            return Ok(result.Value);
         }
     }
 }
